f9c87298bc872858a0f91c40dee2fd64eeb2fa40
ckrueger1 <charlie.krueger@campus.tu-berlin.de>
2017-06-23 09:57:23 +0800

    rep2

--- py_src/execute_all.sh
+++ py_src/execute_all.sh
@@ -4,8 +4,8 @@
 #	[[ $(python 0_crawl doctor | tail -1) == "Crawling done." ]] && break
 #done
 #while true; do	[[ $(python 0_crawl faq | tail -1) == "Crawling done." ]] && break; done
-python 1_parse
-python 2_intermediate
-python 31_pagerank
-python 32_qscore
-python 4_index
+#python 1_parse
+#python 2_intermediate
+python 31_pagerank \
+&& python 32_qscore \
+&& python 4_index
